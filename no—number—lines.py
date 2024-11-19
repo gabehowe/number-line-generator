b6c537cdb3872abd63be16f34efe16a51cabf4e1
Gabriel Howe <me@gabe.how>
2024-11-18 23:31:40 -0500

    remove .idea and add gitignore

--- no—number—lines.py
+++ no—number—lines.py
@@ -1,19 +1,26 @@
 import re
 from math import floor
+from typing import Optional
 
 while True:
     while True:
         inp = input("Equation (x >= z): ")
-        if re.match('^.[\s]?(>=|<=|<|>)[\s]?[-]?\d*$', inp):
+        if re.match('^[a-z0-9]{1,4}[\s]?(>=|<=|<|>)[\s]?[-]?.*[\s]?((>=|<=|<|>)[\s]?.*)?$', inp):
             break
 
     first_var = re.findall('^\w', inp)[0]
     second_var = int(re.findall('[-]?[\d]*$', inp)[0])
-    operand = re.findall('(>=|<=|<|>)', inp)[0]
-    normal_chars = {'start': '├', 'middle': '─', 'line': '┼', 'end': '┤', 'section': '────┼'}
-    full_chars = {'start': '╠', 'middle': '═', 'line': '╬', 'end': '╣', 'section': '════╬'}
-    space = {1: '    ', 2: '   ', 3: '  '}
-    line_length = 20
+    operand: str = re.findall('(>=|<=|<|>)', inp)[0]
+    # second_operand: str = Optional[re.findall('(>=|<=|<|>)', inp)[1]]
+    # third_var = re.findall('[-]?[\w]*', inp)[1]
+    if first_var.isdigit():
+        first_var = int(first_var)
+    # if third_var.isdigit():
+    #     third_var = int(third_var)
+    normal_chars = {'start': '├', 'middle': '─', 'line': '┼', 'end': '┤', 'section': '─────┼'}
+    full_chars = {'start': '╠', 'middle': '═', 'line': '╬', 'end': '╣', 'section': '═════╬'}
+    space = {1: '     ', 2: '    ', 3: '   ', 4: '  ', 5: ' '}
+    line_length = 14
     line = ''
     nums = ''
     for i in range(floor(second_var - line_length / 2), floor(second_var + line_length / 2) + 1):
